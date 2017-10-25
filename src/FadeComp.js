2e2a2c0d9bf821350be944fa37399ce71baa50c8
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2017-10-25 14:33:45 -0500

    Clean App and FadeComp

--- src/FadeComp.js
+++ src/FadeComp.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import { CSSTransitionGroup } from 'react-transition-group';
 
 const FadeComp = WrappedComponent => class AnimatedComp extends Component {
-	constructor(props) {
-		super(props);
-	}
-
 	render() {
 		return (
 			<CSSTransitionGroup
